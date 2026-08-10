a8f8d9318f881050cfd17ee82a6b5bea05fb1310
João Vitor <joaomoraes26@icloud.com>
2026-08-10 15:37:57 -0300

    Exercicio 4 - Retangulo

---
+++ 04-Retângulo/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main( string [] args)
+    {
+        Retangulo R1 = new Retangulo(25,30);
+        R1.Exibir();
+    }
+}
---
+++ 04-Retângulo/Retangulo.cs
@@ -0,0 +1,32 @@
+using System.Security;
+
+public class Retangulo
+{
+    public double Largura  {get; set;}
+    public double Altura {get; set;}
+
+    public Retangulo(double largura, double altura)
+    {
+        Largura = largura;
+        Altura = altura;
+    }
+
+    public double Area()
+    {
+        return Largura * Altura;
+    }
+
+    public double Perimetro()
+    {
+        return 2 * (Largura + Altura);
+    }
+
+    public void Exibir()
+    {
+        Console.WriteLine("Altura " + Altura);
+        Console.WriteLine("Largura " + Largura);
+        Console.WriteLine("Area : " + Area());
+        Console.WriteLine("Perimetro: " + Perimetro());
+    }
+
+}
